7b1ba003c8b5d451a92c1b40d7d3d8cc7547342b
Christopher Lewis <topher.the.good.ninja@gmail.com>
2025-07-18 10:17:17 -0500

    No blank page!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout.jsx';
 import Home from './pages/Home.jsx';
-import Dashboard from './pages/Dashboard.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
 
@@ -12,16 +11,12 @@ import Signup from './pages/Signup.jsx';
 function App() {
     // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-
-
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout><Dashboard /></Layout>} />
-                <Route path="/login" element={<Layout><Login /></Layout>} />
-                <Route path="/signup" element={<Layout><Signup /></Layout>} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout><Home /></Layout>} />
+            <Route path="/login" element={<Layout><Login /></Layout>} />
+            <Route path="/signup" element={<Layout><Signup /></Layout>} />
+        </Routes>
     )
 }
 
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+
+    return (
+        <div>
+            <h2>Welcom, </h2>
+        </div>
+    )
+}
